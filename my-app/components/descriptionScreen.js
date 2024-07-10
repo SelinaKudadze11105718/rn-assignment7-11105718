9d5d42593fc7564435ac55e5db5f6e9e66caefe1
SelinaKudadze11105718 <seakudadze001@st.ug.edu.gh>
2024-07-10 23:38:23 +0000

    Description Screen edited

--- my-app/components/descriptionScreen.js
+++ my-app/components/descriptionScreen.js
@@ -15,21 +15,36 @@ export default function DescriptionScreen(){
   });
    
     return(
-        <ScrollView style={{backgroundColor:'white'}}>
+        <View style={{backgroundColor:'white'}}>
             <TopHomeScreen/>
 
             <View style={{marginTop:20,height:100}}>
             
-                    <Image source={{uri : image}} style={{height:450,weight:450,borde:1,borderRadius:30}} />
-                    <Text style={styles.name}>{title}</Text>
-                    <Text> {description.substring(0,50)}</Text>
+                    <Image source={{uri : image}} style={{height:350,weight:350,borde:1,borderRadius:30}} />
+                    <Text style={styles.name}>
+                        {title.substring(0,20)}
+                        {'\n'}
+                        {title.substring(20,40)}
+                        {'\n'}
+                        {title.substring(40)}
+                        
+                       </Text>
+                    <Image source={require('../assets/Export.png')} style={{position:'absolute',left:340,top:420}}/>
+                    <Text style={{marginRight:20,fontFamily:'Quicksand',fontSize:14}}> {description.substring(0,50)}</Text>
                     <Text style={styles.price}>${price}</Text>
+                    <Text style={{textAlign:'left',marginLeft:10,fontSize:15,fontFamily:'Quicksand-Bold'}}>DESCRIPTION</Text>
                     <Text style={styles.description}>{description}</Text>
                     
             </View>
+            <View style={styles.bottom}>
+                <Image source={require('../assets/Plus.png')}/>
+                <Text style={{color:'white',fontFamily:'Quicksand',textAlign:'center'}}>ADD TO BASKET </Text>
+                <Image source={require('../assets/Heart.png')} style={{position:'absolute',left:340}}/>
+
+            </View>
             
             
-        </ScrollView>
+        </View>
     )
 }
 
@@ -39,16 +54,17 @@ const styles= StyleSheet.create({
     name:{
         fontWeight: 'bold',
         marginBottom:10,
-        fontSize:20,
+        fontSize:18,
         marginTop:10,
-        textAlign: 'center',
+        textAlign: 'left',
         fontFamily: 'Quicksand-Bold',
+        marginLeft:10,
 
     },
     description:{
-        textAlign: 'justify',
+        textAlign: 'left',
         left:10,
-        right:20,
+        marginRight:40,
         fontFamily: 'Quicksand-Regular',
         marginBottom:10,
         fontSize:16,
@@ -61,7 +77,16 @@ const styles= StyleSheet.create({
         marginLeft:5,
         fontSize:20,
         marginTop:10,
-        textAlign: 'center',
+        textAlign: 'left',
         fontFamily: 'Quicksand-Regular',
     },
+    bottom:{
+        backgroundColor:'#555555',
+        height:60,
+        width:400,
+        position: 'relative',
+        top:550,
+        flexDirection:'row',
+        gap:40,
+    }
 });
